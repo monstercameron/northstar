de9e65f1506b99531982b90d0085240f9bb95558
James Zacka <jameszacka@gmail.com>
2021-11-07 03:40:30 -0500

    improved chat feature by changing message ui.

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import Style from './index.module.css'
 import { StreamChat } from 'stream-chat';
 import { 
+    Attachment,
+    Avatar,
     Chat, 
     Channel, 
-    ChannelHeader, 
+    ChatAutoComplete,
+    ChannelHeader,
+    EmojiPicker,
     MessageInput, 
-    MessageList, 
+    MessageList,
+    SendButton, 
     Thread,
     useMessageContext,
-    Window 
+    UploadsPreview,
+    Window, 
+    useChannelStateContext
 } from 'stream-chat-react';
+import  {
+    FileUploadButton, ImageDropzone
+} from 'react-file-utils'
+
+import { useDropzone } from 'react-dropzone';
 
 import 'stream-chat-react/dist/css/index.css';
 
@@ -21,6 +34,7 @@ chatClient.connectUser(
     id: 'aged-fire-8',
     name: 'aged',
     image: 'https://getstream.io/random_png/?id=aged-fire-8&name=aged',
+    isSender: true
   },
   userToken,
 );
@@ -32,13 +46,47 @@ const channel = chatClient.channel('messaging', 'custom_channel_id', {
   members: ['aged-fire-8'],
 });
 
+// Displays message
+const CustomMessage = () => {
+    const { message } = useMessageContext();
+
+    var sender = message.user.isSender;
+
+    const customMessage = message.user.isSender == true ? (
+        <div className={`row`}>
+            <div>
+                <div className={`${Style.senderbubble}`}>
+                    {message.text}
+                    {message.attachments && <Attachment attachments={message.attachments} />}
+                </div>
+            </div>
+        </div>
+    ) :
+    (
+        <div className={`row`}>
+            <div>
+                <div className={`${Style.receiverbubble}`}>
+                    {message.text}
+                    {message.attachments && <Attachment attachments={message.attachments} />}
+                </div>
+            </div>
+        </div>
+    );
+  
+    return (
+        customMessage
+    );
+  };
+
 const App = () => (
   <Chat client={chatClient} theme='messaging light'>
-    <Channel channel={channel}>
+    <Channel 
+        channel={channel} 
+        TypingIndicator={() => null}>
       <Window>
         <ChannelHeader />
-        <MessageList/>
-        <MessageInput />
+        <MessageList Message={CustomMessage}/>
+        <MessageInput/>
       </Window>
       <Thread />
     </Channel>
